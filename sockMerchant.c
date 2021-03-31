80de59f2ba3010ccd4184524f613cd621cb7eb90
mallocLevy <berkercaner1@gmail.com>
2021-03-31 11:12:26 +0300

    c solution for sockmerchant problem from hackerrank

---
+++ sockMerchant.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+#define HASH_SIZE 100
+
+/*
+	c solution for sock merchant problem from hackerrank.com
+	problems URL = https://hackerrank.com/challenges/sock-merchant/problem
+	time complexity = O(n)
+*/
+
+
+int sockMerchant(int n, int ar_count, int* ar){
+	int *hashArr = calloc(sizeof(int),HASH_SIZE);
+	int pairs = 0;
+	
+	for(int i=0; i<n; i++){
+		if(hashArr[ar[i]-1] == 0)
+			hashArr[ar[i]-1] = ar[i];
+		else{  // if pairs match, clean it to count if there will be other pair with same value
+			pairs++;
+			hassArr[ar[i]-1] = 0;
+		}
+	}
+	return pairs;
+}
